940d61b36407aba0f9f898ae8ef8fb57503e3cb3
Todd Wolfson <todd@twolfson.com>
2012-02-05 20:11:13 -0800

    Finished cleaning up collector.js

---
+++ src-test/collector.bdd_skeleton.js
@@ -0,0 +1,17 @@
+// Test url's
+// Relative
+// Absolute
+// Foreigh
+
+// Inline elements
+// Script in head
+// Script in body
+// Stylesheet in head
+// Stylesheet in body?
+
+// CSS url's
+// Relative path -> absolute path
+// Absolute path -> absolute path
+
+// Self
+// self...
--- src/collector.js
+++ src/collector.js
@@ -11,7 +11,8 @@
   } else {
     this[name] = definition;
   }
-}('ResourceCollector', (function () {
+}('ResourceCollector', (function (document) {
+/*** BEGIN: Helper objects/functions ***/
 function noop() {}
 
 function Set() {
@@ -23,7 +24,7 @@ Set.prototype = {
     if (!key) {
       return;
     }
-    set[key] = 1;
+    this._set[key] = 1;
   },
   // Late-binding array creator
   'items': function () {
@@ -43,184 +44,48 @@ Set.prototype = {
   }
 };
 
-/**
- * Resource collector constructor function
- * @constructor
- * @param {Object} [options] Options of what to watch on the resources. By default, everything is enabled. Once options is specified, it turns into a whitelist.
- * @param {Boolean} [options.inline=false] Collect resources from inline elements - <elt src=''>
- * @param {Boolean} [options.css=false] Collect resources from CSS urls - * { property: url('') }
- * @param {Boolean} [options.self=false] Collect own url
- * @param {Boolean} [options.sameDomainOnly=true] Only collect resources from the same domain
- * @returns {Object<ResourceCollector>} A resource collector object
- */
-function ResourceCollector(options) {
-  // Fallback options
-  options = options || {'inline': true, 'css': true, 'self': true, 'sameDomainOnly': true};
-
-  // Set up options on the resource collector
-  this._inline = options.inline || false;
-  this._css = options.css || false;
-  this._self = options.self || false;
-  this._sameDomain = options.sameDomainOnly || true;
-}
+// TODO: Objectify URL's?
 
 /**
- * Instantiates a ResourceCollector, collects, and callsback with results
- * @see ResourceCollector and ResourceCollector.prototype.collect
+ * Helper method that checks if a url is absolute or relative
+ * @param {String} url Url to check on
+ * @returns {Boolean} True if the url is absolute, false otherwise.
  */
-ResourceCollector.collect = function (options) {
-  var rc = new ResourceCollector(options);
-  return rc.collect(callback);
+function isAbsoluteUrl(url) {
+  return !!(url.match(/([^:]*:)?\/\//));
 }
-
-var htmlElt = document.documentElement;
-ResourceCollector.prototype = {
-  /**
-   * Collect all inline items
-   * @returns {String[]} Unfiltered array of inline urls (WARNING: May contain duplicates)
-   */
-  'collectInline': function () {
-    // TODO: Also collect elements from the head (including link href=".css")
-    var docHtml = htmlElt.innerHTML,
-        srcWithExcessArr = docHtml.match(/src=([^>]*>)/g) || [],
-        srcWithExcess,
-        i = 0,
-        len = srcWithExcessArr.length,
-        src,
-        retArr = [];
-
-    // Loop through all the sources
-    for (; i < len; i++) {
-      // Strip the quotes from the source
-      srcWithExcess = srcWithExcessArr[i];
-      src = stripQuotes(srcWithExcess);
-
-      // If there is a src found, append it to the retArr
-      if( src !== false ) {
-        retArr[retArr.length] = srcStr;
-      }
-    }
-
-    return retArr;
-  },
-  /**
-   * Collector function for urls within style sheets. Relative paths are automatically mapped to absolute paths due to the nature of CSS
-   * @returns {String[]} Unfiltered array of CSS urls (WARNING: May contain duplicates)
-   */
-  'collectCss': function () {
-    // Collect them and add to the resource collection
-    var styleSheets = document.styleSheets || [],
-      styleSheet,
-      i = styleSheets.length,
-      rules,
-      j,
-      text,
-      urlMatches,
-      k,
-      urlStr;
-
-    while( i-- ) {
-      styleSheet = styleSheets[i];
-
-      // Collect stylesheet url
-      urlStr = styleSheet.href || '';
-      if( checkUrlRelative(urlStr) ) {
-        // Store stylesheet to resources
-        resources.add(urlStr);
-// TODO: handle relative path in stylesheet
-        // Grab inner urls
-        rules = styleSheet.cssRules || styleSheet.rules || [];
-        j = rules.length;
-        while( j-- ) {
-          text = rules[j].cssText;
-          urlMatches = text.match(/url\([^\)]*\)/g);
-          if( urlMatches ) {
-            k = urlMatches.length;
-            while( k-- ) {
-              var urlStr = grabRelativePath(urlMatches[k], 4);
-              resources.add(urlStr);
-            }
-          }
-        }
-      }
-    }
-  },
-  /**
-   * Collect items with the options attached to this ResourceCollector
-   * @returns {String[]} Array of resources on the page (with the specified options)
-   */
-  'collect': function (callback) {
-    // Collect all resource collection-related options
-    var inline = this._inline,
-        css = this._css,
-        self = this._self,
-    // Set up intermediate variables
-        arr,
-        i,
-        len,
-    // Set up resources set (prevent redundancy)
-        resources = new Set();
-
-    // If we need to collect inline elements
-    if (inline === true) {
-      // Collect them and add to the resource collection
-      arr = this.collectInline();
-      for (i = 0, len = arr.length; i < len; i++) {
-        resources.add(arr[i]);
-      }
-    }
-    
-    // If we need to collect css url's
-    if (css === true) {
-    }
-
-    // If we should be tracking this document, add it to the collection
-    if (self === true) {
-      resources.add(location);
-    }
-
-    // Collect all resource filtering options
-    var sameDomain = this._sameDomain,
-        retArr = allResources;
-
-    // If we are to filter by the same domain
-    if (sameDomain === true) {
-      // Reset retArr
-      retArr = [];
-
-      // Iterate each item
-
-    }
-  }
-};
-
-// TODO: Objectify URL's
 
 var host = location.hostname;
-function checkUrlRelative(url) {
-  var relative = true,
+/**
+ * Helper method that checks if a url is on the same domain (needs some robustification with different subdomains)
+ * @param {String} url Url to check on
+ * @returns {Boolean} True if the url has the same domain, false otherwise.
+ */
+function isSameDomain(url) {
+  // Assume the url is relative to start
+  var retBool = true,
       urlHostArr,
-      _host;
+      urlHost;
 
   // If the url is absolute
-  if( url.match(/([^:]*:)?\/\//) ) {
-    relative = false;
+  if (isAbsoluteUrl(url)) {
+    // Assume different domain since absolute
+    retBool = false;
 
     // Get hostname
     urlHostArr = url.match(/\/\/([^\/]*)/);
-    if( urlHostArr ) {
-      _host = host;
+    if (urlHostArr) {
       urlHost = urlHostArr[1];
 
       // TODO: Robustify this? (www. vs www2. will not match)
       // Check if one is a subdomain of the other (this also accounts for same domain)
-      if( _host.indexOf( urlHost ) !== -1 || urlHost.indexOf(_host) !== -1 ) {
-        relative = true;
+      if( host.indexOf(urlHost) !== -1 || urlHost.indexOf(host) !== -1 ) {
+        retBool = true;
       }
     }
   }
 
-  return relative;
+  return retBool;
 }
 
 /**
@@ -265,13 +130,216 @@ function stripQuotes(str) {
   return url;
 }
 
-// Run collecter onces dom is ready
-domready(function(){
-  // In styleSheets, find all url()
-  if( watchCssUrls ) {
-    
+/**
+ * Takes a filename and returns the base directory
+ * @param {String} filename Name of file to get base directory from
+ * @returns {String} Base directory of file
+ */
+function basePath(filename) {
+  var lastSlashIndex = filename.lastIndexOf('/'),
+  // The return string will include the slash (if there is none, it will truncate to an empty string)
+      retStr = filename.slice(0, lastSlashIndex + 1);
+  return retStr;
+}
+
+/*** END: Helper objects/functions ***/
+/*** BEGIN: ResourceCollector core ***/
+
+/**
+ * Resource collector constructor function
+ * @constructor
+ * @param {Object} [options] Options of what to watch on the resources. By default, everything is enabled. Once options is specified, it turns into a whitelist.
+ * @param {Boolean} [options.inline=false] Collect resources from inline elements - <elt src=''> and <link href=''>
+ * @param {Boolean} [options.css=false] Collect resources from CSS urls - * { property: url('') }
+ * @param {Boolean} [options.self=false] Collect own url
+ * @param {Boolean} [options.sameDomainOnly=true] Only collect resources from the same domain
+ * @returns {Object<ResourceCollector>} A resource collector object
+ */
+function ResourceCollector(options) {
+  // Fallback options
+  options = options || {'inline': true, 'css': true, 'self': true, 'sameDomainOnly': true};
+
+  // Set up options on the resource collector
+  this._inline = options.inline || false;
+  this._css = options.css || false;
+  this._self = options.self || false;
+  this._sameDomain = options.sameDomainOnly || true;
+}
+
+/**
+ * Instantiates a ResourceCollector, collects, and callsback with results
+ * @see ResourceCollector and ResourceCollector.prototype.collect
+ */
+ResourceCollector.collect = function (options) {
+  var rc = new ResourceCollector(options);
+  return rc.collect();
+};
+
+/**
+ * Function that collects all inline sources from the document
+ * @returns {String[]} Unfiltered array of inline urls (WARNING: May contain duplicates)
+ */
+var htmlElt = document.documentElement;
+ResourceCollector.collectInline = function () {
+  // Collect the link href's and src's seperately
+  var docHtml = htmlElt.innerHTML,
+      hrefWithExcessArr = docHtml.match(/<link[^>]*href=([^>]*>)/g) || [],
+      srcWithExcessArr = docHtml.match(/src=([^>]*>)/g) || [],
+  // Then, concatenate them together
+      urlWithExcessArr = hrefWithExcessArr.concat(srcWithExcessArr),
+      urlWithExcess,
+      i = 0,
+      len = urlWithExcessArr.length,
+      url,
+      retArr = [];
+
+  // Loop through all the sources
+  for (; i < len; i++) {
+    // Strip the quotes from the source
+    urlWithExcess = urlWithExcessArr[i];
+    url = stripQuotes(urlWithExcess);
+
+    // If there is a url found, append it to the retArr
+    if( url !== false ) {
+      retArr[retArr.length] = url;
+    }
   }
 
-  // Execute callback function on resources
-  callback(resources.items());
-});
+  // Return the sources
+  return retArr;
+};
+
+/**
+ * Collector function for urls within style sheets. Relative paths are automatically mapped to absolute paths due to the nature of CSS
+ * @returns {String[]} Unfiltered array of CSS urls (WARNING: May contain duplicates)
+ */
+ResourceCollector.collectCss = function () {
+  // Get the stylesheets attached to the page
+  var stylesheets = document.styleSheets || [],
+      stylesheet,
+      stylesheetUrl,
+      stylesheetDir,
+      i = 0,
+      len = stylesheets.length,
+  // Set up preparatory variables
+      rules,
+      j,
+      len2,
+      ruleContent,
+      urlWithExcessArr,
+      urlWithExcess,
+      k,
+      len3,
+      url,
+      retArr = [];
+
+  // Iterate each stylesheet
+  for (; i < len; i++) {
+    stylesheet = stylesheets[i];
+
+    // Grab the stylesheet's url (for relative paths)
+    stylesheetUrl = stylesheet.href;
+    stylesheetDir = basePath(stylesheetUrl);
+
+    // Get the stylesheet's rules
+    rules = stylesheet.cssRules || stylesheet.rules || [];
+
+    // Iterate the rules
+    for (j = 0, len2 = rules.length; j < len2; j++) {
+      // Get the rule's content
+      ruleContent = rules[j].cssText;
+
+      // Find any url's mentioned in the css
+      urlWithExcessArr = ruleContent.match(/url\([^\)]*\)/g) || [];
+      for (k = 0, len3 = urlWithExcessArr.length; k < len3; k++) {
+        urlWithExcess = urlWithExcessArr[k];
+        url = stripQuotes(urlWithExcess);
+
+        // If there is a url
+        if (url !== false) {
+          // If the url is not absolute
+          if (!isAbsoluteUrl(url)) {
+            // Prepend the stylesheet location
+            url = stylesheetDir + url;
+          }
+
+          // Add it to the return array
+          retArr[retArr.length] = url;
+        }
+      }
+    }
+  }
+
+  // Return the url's
+  return retArr;
+};
+
+ResourceCollector.prototype = {
+  /**
+   * Collect items with the options attached to this ResourceCollector
+   * @returns {String[]} Array of resources on the page (with the specified options)
+   */
+  'collect': function (callback) {
+    // Collect all resource collection-related options
+    var inline = this._inline,
+        css = this._css,
+        self = this._self,
+    // Set up intermediate variables
+        arr,
+        i,
+        len,
+    // Set up resources set (prevent redundancy)
+        resourceSet = new Set();
+
+    // If we need to collect inline elements
+    if (inline === true) {
+      // Collect them and add to the resource collection
+      arr = ResourceCollector.collectInline();
+      for (i = 0, len = arr.length; i < len; i++) {
+        resourceSet.add(arr[i]);
+      }
+    }
+
+    // If we need to collect css url's
+    if (css === true) {
+      // Collect them and add to the resource collection
+      arr = ResourceCollector.collectCss();
+      for (i = 0, len = arr.length; i < len; i++) {
+        resourceSet.add(arr[i]);
+      }
+    }
+
+    // If we should be tracking this document, add it to the collection
+    if (self === true) {
+      resourceSet.add(location);
+    }
+
+    // Collect all resource filtering options
+    var sameDomain = this._sameDomain,
+        resources = resourceSet.items(),
+        resource,
+        retArr = resources;
+
+    // If we are to filter by the same domain
+    if (sameDomain === true) {
+      // Reset retArr
+      retArr = [];
+
+      // Iterate each item
+      for (i = 0, len = resources.length; i < len; i++) {
+        resource = resources[i];
+
+        // If the url is on the same domain, add it to the retArr
+        if (isSameDomain(resource)) {
+          retArr[retArr.length] = resource;
+        }
+      }
+    }
+
+    return retArr;
+  }
+};
+
+return ResourceCollector;
+}(document))
+));
